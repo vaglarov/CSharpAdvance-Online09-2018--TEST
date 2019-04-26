a775c08fa80e8fa7680f9ae8492045c1ea88a477
vaglarov <pasific@abv.bg>
2019-04-26 08:59:42 +0300

    003

---
+++ 02. Multidimensional-Arrays/VS/003. Primary Diagonal/003. Primary Diagonal.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Linq;
+
+namespace _003._Primary_Diagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberRowAndCol = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[numberRowAndCol, numberRowAndCol];
+            // Fill Matrix
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                var colElements = Console.ReadLine().
+                    Split(" ", StringSplitOptions.RemoveEmptyEntries).
+                    Select(int.Parse).
+                    ToArray();
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = colElements[col];
+                }
+            }
+            var sumLeftDiagonal = default(int);
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sumLeftDiagonal += matrix[i, i];
+            }
+            Console.WriteLine(sumLeftDiagonal);
+        }
+    }
+}
