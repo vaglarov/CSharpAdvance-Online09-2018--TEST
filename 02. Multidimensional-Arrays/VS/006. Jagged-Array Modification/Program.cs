6247b3af773e031879478d8430d1cdcf66e5c2d6
vaglarov <pasific@abv.bg>
2019-04-27 15:46:39 +0300

    06

---
+++ 02. Multidimensional-Arrays/VS/006. Jagged-Array Modification/Program.cs
@@ -0,0 +1,67 @@
+﻿
+namespace _006._Jagged_Array_Modification
+{
+    using System;
+    using System.Linq;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Creat Array and Fill
+            var inputNumberRow = int.Parse(Console.ReadLine());
+            var jaggedArr = new int[inputNumberRow][];
+            for (int row = 0; row < jaggedArr.Length; row++)
+            {
+                var inputNumbers = Console.ReadLine().
+                    Split(" ", StringSplitOptions.RemoveEmptyEntries).
+                    Select(int.Parse).
+                    ToArray();
+                jaggedArr[row] = new int[inputNumbers.Length];
+                for (int col = 0; col < inputNumbers.Length; col++)
+                {
+                    jaggedArr[row][col] = inputNumbers[col];
+                }
+            }
+
+            // Operation
+            string commandInput = string.Empty;
+            while ((commandInput = Console.ReadLine()) != "END")
+            {
+                var commandInputParts = commandInput.Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+                var command = commandInputParts[0];
+                var rowCorrection = int.Parse(commandInputParts[1]);
+                var colCorrection = int.Parse(commandInputParts[2]);
+                var edditValue = int.Parse(commandInputParts[3]);
+                if (rowCorrection < 0 ||
+                    rowCorrection > jaggedArr.Length - 1 ||
+                    colCorrection < 0 ||
+                    colCorrection > jaggedArr[rowCorrection].Length-1
+                    )
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+                else
+                {
+                    switch (command)
+                    {
+                        case "Add":
+                            jaggedArr[rowCorrection][colCorrection] += edditValue;
+                            break;
+                        case "Subtract":
+                            jaggedArr[rowCorrection][colCorrection] -= edditValue;
+                            break;
+                        default:
+                            Console.WriteLine("Wrong input!");
+                            break;
+                    }
+                }
+            }
+
+            // Print jaggedArr
+            for (int row = 0; row < jaggedArr.Length; row++)
+            {
+                Console.WriteLine(string.Join(" ",jaggedArr[row]));
+            }
+        }
+    }
+}
