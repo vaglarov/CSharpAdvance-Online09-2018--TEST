96ac4ee4613efb5b1e58b9affd43b68ebc3faa05
vaglarov <pasific@abv.bg>
2019-04-30 18:06:15 +0300

    Copy program

---
+++ 04. Streams/vs/Streams/104. Copy Binary File/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace _104._Copy_Binary_File
+{
+using System;
+    using System.IO;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sourceFile = @"D:\SoftUni\C# Advanced\04. Streams\resurses\copyMe.png";
+            string destinationFile = @"D:\SoftUni\C# Advanced\04. Streams\resurses\copyMeCopy.png";
+            using (FileStream streamFile=new FileStream(sourceFile, FileMode.Open) )
+            {
+                var size = streamFile.Length;
+                byte[] buffer = new byte[size];
+                streamFile.Read(buffer, 0, buffer.Length);
+                using (FileStream writeFileCopy = new FileStream(destinationFile, FileMode.Create))
+                {
+                    writeFileCopy.Write(buffer, 0, buffer.Length);
+                }
+            }
+        }
+    }
+}
