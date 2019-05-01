07074cccab955dfec6cbb683294247e0b39a4381
vaglarov <pasific@abv.bg>
2019-05-01 09:32:07 +0300

    push

--- 04. Streams/vs/Streams/105. Slicing File/SlicingFile.cs
+++ 04. Streams/vs/Streams/105. Slicing File/SlicingFile.cs
@@ -25,25 +25,12 @@
                 byte[] buffer = new byte[sizePart];
                 for (int i = 0; i < parts; i++)
                 {
-
                     string destinationDirectoryPart = destinationDirectory + $" Part {i}.mp4";
                     listParts.Add(destinationDirectoryPart);
-                    int readedBytes = 0;
                     using (FileStream writeNewFile = new FileStream(destinationDirectoryPart, FileMode.Create))
                     {
-                        while (true)
-                        {
-                            int bytesCount = fileStreamRead.Read(buffer, 0, buffer.Length);
-                            if (readedBytes > sizePart)
-                            {
-                                break;
-                            }
-                            if (bytesCount == 0)
-                            {
-                                break;
-                            }
-                            writeNewFile.Write(buffer, 0, buffer.Length);
-                        }
+                        fileStreamRead.Read(buffer, 0, buffer.Length);
+                        writeNewFile.Write(buffer, 0, buffer.Length);
                     }
                 }
             }
@@ -55,21 +42,17 @@
             {
                 foreach (var file in files)
                 {
-                    byte[] buffer = new byte[file.Length];
                     using (FileStream readFile = new FileStream(file, FileMode.Open))
                     {
-                        while (true)
-                        {
-                            int bytesCount = readFile.Read(buffer, 0, buffer.Length);
-                            if (bytesCount == 0)
-                            {
-                                break;
-                            }
-                            writeFile.Write(buffer);
-                        }
+                        var size = readFile.Length;
+                        byte[] buffer = new byte[size];
+                        readFile.Read(buffer, 0, buffer.Length);
+                        writeFile.Write(buffer);
                     }
                 }
             }
+
         }
     }
 }
+
