40affc3025159d88a841a0f0797388f05d6ae7b5
vaglarov <pasific@abv.bg>
2019-05-02 14:18:26 +0300

    new

--- 01. Stacks-And-Queues/VS/Stacks-And-Queues/109.  Simple Text Editor/SimpleTextEditor.cs
+++ 01. Stacks-And-Queues/VS/Stacks-And-Queues/109.  Simple Text Editor/SimpleTextEditor.cs
@@ -1,12 +1,58 @@
-﻿using System;
-
-namespace _109.__Simple_Text_Editor
+﻿namespace _109.__Simple_Text_Editor
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
     class SimpleTextEditor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Stack<char> stackCharText = new Stack<char>();
+
+            int numberLine = int.Parse(Console.ReadLine());
+            for (int i = 0; i < numberLine; i++)
+            {
+                //ReadCommand
+                var commandInput = Console.ReadLine();
+                var command = int.Parse(commandInput.Substring(0, 1));
+                var argumetn = commandInput.Substring(2);
+                switch (command)
+                {
+                    case 1:
+                        for (int argumentIndex = 0; argumentIndex < argumetn.Length; argumentIndex++)
+                        {
+                            var addChar = argumetn[argumentIndex];
+                            stackCharText.Push(addChar);
+                        }
+                        break;
+                    case 2:
+                        var numberErasesElement = int.Parse(argumetn);
+                        if (numberErasesElement>=stackCharText.Count)
+                        {
+                            numberErasesElement = stackCharText.Count;
+                        }
+                        for (int numberErase = 0; numberErase < numberErasesElement; numberErase++)
+                        {
+                            stackCharText.Pop();
+                        }
+                        break;
+                    case 3:
+                        var indexElement = int.Parse(argumetn);
+                        var result = stackCharText.Reverse().ToList();
+                        Console.WriteLine(result[indexElement-1]);
+                        break;
+                    case 4:
+                        break;
+                    default:
+                        Console.WriteLine("Wrong command!!");
+                        break;
+                }
+
+
+
+            }
+
         }
     }
 }
---
+++ 01. Stacks-And-Queues/VS/Stacks-And-Queues/1091.  Simple Text Editor/SimpleTextEditor.cs
@@ -0,0 +1,77 @@
+﻿namespace _1091.__Simple_Text_Editor
+{
+    using System;
+    using System.Collections.Generic;
+
+    class SimpleTextEditor
+    {
+        static void Main(string[] args)
+        {
+            int numberLineInput = int.Parse(Console.ReadLine());
+            Stack<string> stackWords = new Stack<string>();
+            for (int i = 0; i < numberLineInput; i++)
+            {
+                //ReadCommand
+                var commandInput = Console.ReadLine();
+                var command = int.Parse(commandInput.Substring(0, 1));
+                var argumetn = string.Empty;
+                if (commandInput.Length > 1)
+                {
+                    argumetn = commandInput.Substring(2);
+                }
+                switch (command)
+                {
+                    case 1:
+                        var word = string.Empty;
+                        if (stackWords.Count == 0)
+                        {
+                            word = argumetn;
+                            stackWords.Push(word);
+                        }
+                        else
+                        {
+                            word = stackWords.Peek();
+                            word += argumetn;
+                            stackWords.Push(word);
+                        }
+                        break;
+                    case 2:
+                        var numberErasesElement = int.Parse(argumetn);
+                        word = stackWords.Peek();
+                        if (numberErasesElement>word.Length)
+                        {
+                            numberErasesElement = word.Length;
+                        }
+                        word = word.Substring(0, (word.Length - (numberErasesElement)));
+                        stackWords.Push(word);
+                        break;
+                    case 3:
+                        var numberIndex = int.Parse(argumetn) - 1;
+                        word = stackWords.Peek();
+                        if (numberIndex > word.Length - 1)
+                        {
+                            Console.WriteLine("Index out of rannge! Please try again");
+                        }
+                        else
+                        {
+                            for (int index = 0; index < word.Length; index++)
+                            {
+                                if (index == numberIndex)
+                                {
+                                    Console.WriteLine(word[index]);
+                                    break;
+                                }
+                            }
+                        }
+                        break;
+                    case 4:
+                        stackWords.Pop();
+                        break;
+                    default:
+                        Console.WriteLine("Wrong command!!");
+                        break;
+                }
+            }
+        }
+    }
+}
