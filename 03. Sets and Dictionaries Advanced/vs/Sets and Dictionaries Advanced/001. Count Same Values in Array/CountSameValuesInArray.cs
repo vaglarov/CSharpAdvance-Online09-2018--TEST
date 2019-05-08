aa155128d17e3d6d09250af1525d71f8601b9e81
vaglarov <pasific@abv.bg>
2019-05-08 06:18:04 +0300

    Dictionary

---
+++ 03. Sets and Dictionaries Advanced/vs/Sets and Dictionaries Advanced/001. Count Same Values in Array/CountSameValuesInArray.cs
@@ -0,0 +1,35 @@
+﻿namespace _001._Count_Same_Values_in_Array
+{
+using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class CountSameValuesInArray
+    {
+        static void Main(string[] args)
+        {
+            var inputArray = Console.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries).Select(double.Parse).ToList();
+            var dictCountValues = new Dictionary<double, int>();
+
+            ///Fill dictionary
+
+            foreach (var input in inputArray)
+            {
+                if (!dictCountValues.ContainsKey(input))
+                {
+                    dictCountValues.Add(input, 1);
+                }
+                else
+                {
+                    dictCountValues[input]++;
+                }
+            }
+
+            //Print dict
+            foreach (var kvp in dictCountValues)
+            {
+                Console.WriteLine($"{kvp.Key} - {kvp.Value} times");
+            }
+        }
+    }
+}
