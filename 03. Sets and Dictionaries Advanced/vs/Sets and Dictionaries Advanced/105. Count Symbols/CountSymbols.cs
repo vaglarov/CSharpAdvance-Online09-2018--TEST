5d810f9c3dd5db8df8bdddf25ab9fdd3ed9fffa9
vaglarov <pasific@abv.bg>
2019-05-10 07:01:40 +0300

    new

---
+++ 03. Sets and Dictionaries Advanced/vs/Sets and Dictionaries Advanced/105. Count Symbols/CountSymbols.cs
@@ -0,0 +1,34 @@
+﻿namespace _105._Count_Symbols
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class CountSymbols
+    {
+        static void Main()
+        {
+            var dictCharAppear = new Dictionary<char, int>();
+            var inputLine = Console.ReadLine().ToCharArray();
+            //Add in dictionary
+            foreach (var charackter in inputLine)
+            {
+                if (!dictCharAppear.ContainsKey(charackter))
+                {
+                    dictCharAppear.Add(charackter, 1);
+                }
+                else
+                {
+                    dictCharAppear[charackter]++;
+                }
+            }
+
+            //Print
+            Console.ForegroundColor = ConsoleColor.Red;
+            foreach (var kvp in dictCharAppear.OrderByDescending(x=>x.Value))
+            {
+                Console.WriteLine($"{kvp.Key}: {kvp.Value} time/s");
+            }
+        }
+    }
+}
---
+++ 03. Sets and Dictionaries Advanced/vs/Sets and Dictionaries Advanced/106. Wardrobe/Wardrobe.cs
@@ -0,0 +1,58 @@
+﻿namespace _106._Wardrobe
+{
+    using System;
+    using System.Collections.Generic;
+
+    class Wardrobe
+    {
+        static void Main(string[] args)
+        {
+            var dictWadrobe = new Dictionary<string, Dictionary<string, int>>();
+            var numberLineInput = int.Parse(Console.ReadLine());
+            //Fill dictionary
+            for (int i = 0; i < numberLineInput; i++)
+            {
+                var separator = new string[] { " -> ", "," };
+                var inputSplitLine = Console.ReadLine().Split(separator, StringSplitOptions.RemoveEmptyEntries);
+                var colorBox = inputSplitLine[0];
+                if (!dictWadrobe.ContainsKey(colorBox))
+                {
+                    dictWadrobe.Add(colorBox, new Dictionary<string, int>());
+                }
+                for (int index = 1; index < inputSplitLine.Length; index++)
+                {
+                    var itemToAdd = inputSplitLine[index];
+                    if (!dictWadrobe[colorBox].ContainsKey(itemToAdd))
+                    {
+                        dictWadrobe[colorBox].Add(itemToAdd, 1);
+                    }
+                    else
+                    {
+                        dictWadrobe[colorBox][itemToAdd]++;
+                    }
+
+                }
+            }
+
+            var itemToFind = Console.ReadLine().Split();
+            //Print result
+            Console.ForegroundColor = ConsoleColor.Red;
+            foreach (var colorBox in dictWadrobe)
+            {
+                Console.WriteLine($"{colorBox.Key} clothes:");
+                foreach (var typeOfDress in colorBox.Value)
+                {
+                    if (itemToFind[0]==colorBox.Key&&itemToFind[1]==typeOfDress.Key)
+                    {
+                        Console.WriteLine($"* {typeOfDress.Key} - {typeOfDress.Value} (found!)");
+                    }
+                    else
+                    {
+
+                    Console.WriteLine($"* {typeOfDress.Key} - {typeOfDress.Value}");
+                    }
+                }
+            }
+        }
+    }
+}
